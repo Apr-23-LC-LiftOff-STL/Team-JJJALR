1f31972b6a8c9b9ee875b7b9d114f9e44f2ee840
julian <julianschriedel@gmail.com>
2023-06-07 16:22:33 -0500

    maybe finished update profile method and routing; about to create corresponding ngForm

--- MyHabit/MyHabit/src/main/ui/src/app/edit-profile/edit-profile.component.ts
+++ MyHabit/MyHabit/src/main/ui/src/app/edit-profile/edit-profile.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ProfileEdit } from './edit-profile';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { EditProfileService } from './edit-profile.service';
+import { error } from 'console';
 @Component({
   selector: 'app-edit-profile',
   templateUrl: './edit-profile.component.html',
@@ -9,14 +11,17 @@ import { Router } from '@angular/router';
 })
 export class EditProfileComponent implements OnInit {
 
-  // declare/initialize fields?
-
+  // PROPERTIES
+  // call ProfileEdit interface
   public profileEdit!: ProfileEdit;
   model: any = {};
 
-  constructor(
-    private http: HttpClient,
-    private router: Router) { }
+
+  // CONSTRUCTORS
+  constructor(private editProfileService: EditProfileService, private http: HttpClient, private router: Router) {
+    this.http = http;
+    this.router = router;
+  }
 
   ngOnInit(): void {
     if (!localStorage.authToken) {
@@ -31,34 +36,59 @@ export class EditProfileComponent implements OnInit {
     }
   }
 
-  // EDIT PROFILE METHODS
+  
+  // METHODS:
+
+  // UPDATE PROFILE INFO
+  // onUpdateProfile takes in editPorfileForm as paramenter, returns nothing (void)
+  public onUpdateProfile(editProfileForm: ngForm): void {
+    // call updateProfile method from editProfileService
+    // takes in values from profile form (editProfileForm.value) as parameters
+    // .subscribe listens for the appropriate event to execute the fxn in ()
+    this.editProfileService.updateProfile(editProfileForm.value).subscribe(
+      // assign ProfileEdit values to response variable
+      (response: ProfileEdit) => {
+        // when event occurs, .subscribe will print response
+        console.log(response);
+        // if a user's id (I picked an arbitrary field) is not undefined
+        if (response.id != 0) {
+          // then return the user to their profile???
+          this.router.navigate([''])
+        }
+      },
+      // if an error occurs, print error message
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    )
+  };
 
   // addProfile method is encompassed in addUsers method in AuthenticationController
   // users create a profile upon registration
 
-  updateProfile() {
-    let url = 'http://localhost:8080/profile/update/info';
-    let profileData = {
-      userId: this.model.userId,
-      firstName: this.model.firstName,
-      lastName: this.model.lastName,
-      displayName: this.model.displayName,
-      email: this.model.email,
-      location: this.model.location,
-      status: this.model.status
-    }
-    console.log(profileData)
-    // this.http.put<any>(url, profileData).subscribe(res =>{
-    //   console.log("test")
-    //   console.log(res)
-    //   if(res.id !== 0){
-    //     localStorage.setItem('authToken', res.id)
-    //     console.log(localStorage.authToken)
-    //      this.router.navigate(["homepage"])
-    //   }
-    // }),
-    // console.log()
-  }
+  // updateProfile() {
+  //   let url = 'http://localhost:8080/profile/update/info';
+  //   let profileData = {
+  //     userId: this.model.userId,
+  //     firstName: this.model.firstName,
+  //     lastName: this.model.lastName,
+  //     displayName: this.model.displayName,
+  //     email: this.model.email,
+  //     location: this.model.location,
+  //     status: this.model.status
+  //   }
+  //   console.log(profileData)
+  // this.http.put<any>(url, profileData).subscribe(res =>{
+  //   console.log("test")
+  //   console.log(res)
+  //   if(res.id !== 0){
+  //     localStorage.setItem('authToken', res.id)
+  //     console.log(localStorage.authToken)
+  //      this.router.navigate(["homepage"])
+  //   }
+  // }),
+  // console.log()
+  // }
 
   // updateProfilePic() {
   //   let url = 'http://localhost:8080/profile/update/info';
--- MyHabit/MyHabit/src/main/ui/src/app/edit-profile/edit-profile.service.ts
+++ MyHabit/MyHabit/src/main/ui/src/app/edit-profile/edit-profile.service.ts
@@ -1,8 +1,53 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { environment } from "src/environments/environment";
+import { ProfileEdit } from "./edit-profile";
 
 @Injectable(
     { providedIn: 'root' }
 )
 
 export class EditProfileService {
+    private javaServerUrl = environment.devServerUrl;
+
+    constructor(private http: HttpClient) { }
+
+    // EDIT PROFILE METHODS
+
+    // // display user profile
+    // public displayUserProfile(id: number): Observable<???> {
+    //     return this.http.get<???> (`${this.javaServerUrl}/profile/${id}`);
+    // }
+
+    // update existing profile (personal information ONLY)
+    public updateProfile(profileEdit: ProfileEdit): Observable<ProfileEdit> {
+        return this.http.put<ProfileEdit>(`${this.javaServerUrl}/profile/update/info`, profileEdit);
+    }
+
+    // // update profile picture
+    // public updateProfilePic() {
+    //     return this.http.put<ProfileEdit>(`${this.javaServerUrl}/profile/update/info`, profileEdit);
+    // }
+
+    // // update status
+    // public updateStatus() {
+    //     return this.http.put<ProfileEdit>(`${this.javaServerUrl}/profile/update/info`, profileEdit);
+    // }
+
+    // // update bio
+    // public updateBio() {
+    //     return this.http.put<ProfileEdit>(`${this.javaServerUrl}/profile/update/info`, profileEdit);
+    // }
+
+    // // find profile by id
+    // public findUserById(int id) {
+    //     return this.http.put<ProfileEdit>(`${this.javaServerUrl}/profile/update/info`, profileEdit);
+    // }
+
+    // // delete profile
+    // public deleteUser(id: number): Observable<void>  {
+    //     return this.http.delete<void>(`${this.javaServerUrl}/profile/delete/${id}`);
+    // }
+
 }
