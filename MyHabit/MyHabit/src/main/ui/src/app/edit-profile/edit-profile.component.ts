e833dae1095e70dd37197affabaac74287bef5a8
julian <julianschriedel@gmail.com>
2023-06-09 12:14:05 -0500

    refactoring edit-profile service

--- MyHabit/MyHabit/src/main/ui/src/app/edit-profile/edit-profile.component.ts
+++ MyHabit/MyHabit/src/main/ui/src/app/edit-profile/edit-profile.component.ts
@@ -21,127 +21,30 @@ import { NgForm } from '@angular/forms';
 export class EditProfileComponent implements OnInit {
 
   // PROPERTIES
-  // call ProfileEdit interface
   public profileEdit!: ProfileEdit;
-  model: any = {};
 
 
   // CONSTRUCTORS
-  constructor(private http: HttpClient, private router: Router) { }
-
-  // constructor(private editProfileService: EditProfileService, private http: HttpClient, private router: Router) {
-  //   this.http = http;
-  //   this.router = router;
-  // }
+  constructor(private editProfileService: EditProfileService, private http: HttpClient, private router: Router) {
+    this.http = http;
+    this.router = router;
+  }
 
   ngOnInit(): void {
-    // on initialization, if authToken (userId) doesn't exist
-    if (!localStorage.authToken) {
-      // route to registration form
-      this.router.navigate(['/registration'])
-    } else {
-      // else if authToken is valid (user does exist), go to user's profile
-      let url = `http://localhost:8080/profile/${localStorage.authToken}`
-      // make a get request to user profile url with userId
-      this.http.get<any>(url)
-      // when the get request is made, assign the user's id to "response"
-      .subscribe(response => {
-        // profileEdit now holds the User object for the user logged in
+    // when the component is initialized it will call the functions below
+      this.onUpdateProfile();
+  }
+
+  public onUpdateProfile(editProfileForm:  NgForm): void {
+    this.editProfileService.updateProfile(editProfileForm.value, this.profileEdit.id).subscribe(
+      (response: ProfileEdit) => {
         this.profileEdit = response;
-      })
-    }
-  }
-
-
-  // METHODS:
-
-  // UPDATE PROFILE INFO
-
-  // addProfile method is encompassed in addUsers method in AuthenticationController
-  // users create a profile upon registration
-
-  updateProfile() {
-    let url = 'http://localhost:8080/profile/update/info';
-    let profileData = {
-      userId: this.model.userId,
-      firstName: this.model.firstName,
-      lastName: this.model.lastName,
-      displayName: this.model.displayName,
-      email: this.model.email,
-      location: this.model.location,
-      status: this.model.status
-    }
-    console.log(profileData)
-    this.http.put<any>(url, profileData).subscribe(res => {
-      console.log("test")
-      console.log(res)
-      if (res.id !== 0) {
-        localStorage.setItem('authToken', res.id)
-        console.log(localStorage.authToken)
-        this.router.navigate(["profile"])
+        this.router.navigate(['profile'])
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
       }
-    }),
-      console.log()
+    )
   }
-
-  // onUpdateProfile takes in editPorfileForm as paramenter, returns nothing (void)
-  // public onUpdateProfile(editProfileForm: NgForm): void {
-  //   // call updateProfile method from editProfileService
-  //   // takes in values from profile form (editProfileForm.value) as parameters
-  //   // .subscribe listens for the appropriate event to execute the fxn in ()
-  //   this.editProfileService.updateProfile(editProfileForm.value).subscribe(
-  //     // assign ProfileEdit values to response variable
-  //     (response: ProfileEdit) => {
-  //       // when event occurs, .subscribe will print response
-  //       console.log(response);
-  //       // if a user's id (I picked an arbitrary field) is not undefined
-  //       if (response.id != 0) {
-  //         // then return the user to their profile???
-  //         this.router.navigate(['profile'])
-  //       }
-  //     },
-  //     // if an error occurs, print error message
-  //     (error: HttpErrorResponse) => {
-  //       alert(error.message);
-  //     }
-  //   )
-  // };
-
-  // updateProfilePic() {
-  //   let url = 'http://localhost:8080/profile/update/info';
-  //   let picData = {
-  //     userId: this.model.userId,
-  //     picUrl: this.model.picUrl
-  //   }
-  //   console.log(picData)
-  // }
-
-  // updateStatus() {
-  //   let url = 'http://localhost:8080/profile/update/status';
-  //   let statusData = {
-  //     userId: this.model.userId,
-  //     status: this.model.status
-  //   }
-  //   console.log(statusData)
-  // }
-
-  // updateBio() {
-  //   let url = 'http://localhost:8080/profile/update/bio';
-  //   let bioData = {
-  //     userId: this.model.userId,
-  //     bio: this.model.bio
-  //   }
-  //   console.log(bioData)
-  // }
-
-  // findUserById() {
-  //   let url = 'http://localhost:8080/profile/${localStorage.authToken}';
-  //   let userId = this.model.userId;
-  // }
-
-  // deleteUser() {
-  //   let url = 'http://localhost:8080/profile/delete/${localStorage.authToken}';
-  //   let userId = this.model.userId;
-  // }
 
 }
