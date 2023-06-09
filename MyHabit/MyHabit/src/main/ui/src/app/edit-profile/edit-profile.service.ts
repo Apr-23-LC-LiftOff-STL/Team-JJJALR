705a5262e2c77871bb9be36010eed801af4a3a38
julian <julianschriedel@gmail.com>
2023-06-09 09:14:35 -0500

    refactor front end user-profile component and service

--- MyHabit/MyHabit/src/main/ui/src/app/edit-profile/edit-profile.service.ts
+++ MyHabit/MyHabit/src/main/ui/src/app/edit-profile/edit-profile.service.ts
@@ -25,42 +25,8 @@ export class EditProfileService {
 
     // EDIT-PROFILE METHODS
     // update existing profile (personal information ONLY)
-    public updateProfile(profileEdit: ProfileEdit): Observable<ProfileEdit> {
+    public updateProfile(profileEdit: ProfileEdit,  id:  number): Observable<ProfileEdit> {
         return this.http.put<ProfileEdit>(`${this.javaServerUrl}/profile/${id}/update/info`, profileEdit);
     }
-
-
-    // TODO: MOVE TO THEIR OWN INDIVIDUAL COMPONENTS - need their own forms
-    // // update profile picture
-    // public updateProfilePic(): Observable<any> {
-    //     return this.http.put<any>(`${this.javaServerUrl}/profile/${id}/update/picture`, Observable);
-    // }
-
-    // // update status
-    // public updateStatus(): Observable<any>  {
-    //     return this.http.put<any>(`${this.javaServerUrl}/profile/${id}/update/status`, Observable);
-    // }
-
-    // // update bio
-    // public updateBio(): Observable<any>  {
-    //     return this.http.put<any>(`${this.javaServerUrl}/profile/${id}/update/bio`, Observable);
-    // }
-
-
-    // MOVE TO USER SERVICE COMPONENT
-    // ** insert findAllUsers method somewhere
-    //
-    // // find profile by id
-    // public findUserById(id: number): Observable<any>  {
-    //     return this.http.get<any>(`${this.javaServerUrl}/profile/${id}`);
-    // }
-    // display user profile
-    // public displayUserProfile(id: number): Observable<any> {
-    //     return this.http.get<any> (`${this.javaServerUrl}/profile/${id}`, Observable);
-    // }
-    // // delete user
-    // public deleteUser(userId: number): Observable<void>  {
-    //     return this.http.delete<void>(`${this.javaServerUrl}/profile/delete/${id}`);
-    // }
 
 }
