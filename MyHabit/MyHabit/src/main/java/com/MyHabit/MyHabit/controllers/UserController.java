7d00b5f63eaaf04f9ebf45bdf544fc7506b3cc97
julian <julianschriedel@gmail.com>
2023-06-09 00:02:40 -0500

    refactored methods in UserService and UserController; updated paths in UserController; added findAll method in UserService and UserController for potential use in search function

--- MyHabit/MyHabit/src/main/java/com/MyHabit/MyHabit/controllers/UserController.java
+++ MyHabit/MyHabit/src/main/java/com/MyHabit/MyHabit/controllers/UserController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/profile")
 public class UserController {
@@ -21,40 +23,18 @@ public class UserController {
   }
 
   // METHODS
+  // add new profile
+  @PostMapping("/add")
+  public ResponseEntity<Users> addProfile(@RequestBody Users user) {
+    Users newProfile = userService.addProfile(user);
+    return new ResponseEntity<>(newProfile, HttpStatus.CREATED);
+  }
+
   // view user's profile
   @GetMapping("/{id}")
   public ResponseEntity<Users> displayUserProfile(@PathVariable("id") int userId) {
     Users user = userService.findUserById(userId);
     return new ResponseEntity<>(user, HttpStatus.OK);
-  }
-
-  // (POTENTIAL BUTTONS)
-  // update profile information
-  @PutMapping("/update/info")
-  public ResponseEntity<Users> updateProfile(@RequestBody Users user, String first, String last, String display, String email, String location) {
-    Users updatedProfile = userService.updateProfile(user, first, last, display, email, location);
-    return new ResponseEntity<>(updatedProfile, HttpStatus.OK);
-  }
-
-  // update profile picture
-  @PutMapping("/update/picture")
-  public ResponseEntity<Users> updatePicture(@RequestBody Users user, String pic) {
-    Users newPic = userService.updateProfilePic(user, pic);
-    return new ResponseEntity<>(newPic, HttpStatus.OK);
-  }
-
-  // update status
-  @PutMapping("/update/status")
-  public ResponseEntity<Users> updateStatus(@RequestBody Users user, String status) {
-    Users newStatus = userService.updateStatus(user, status);
-    return new ResponseEntity<>(newStatus, HttpStatus.OK);
-  }
-
-  // update profile bio
-  @PutMapping("/update/bio")
-  public ResponseEntity<Users> updateBio(@RequestBody Users user, String bio) {
-    Users newBio = userService.updateBio(user, bio);
-    return new ResponseEntity<>(newBio, HttpStatus.OK);
   }
 
   // delete user
@@ -63,6 +43,73 @@ public class UserController {
     userService.deleteUser(userId);
     return new ResponseEntity<>(HttpStatus.OK);
   }
+
+  // (POTENTIAL BUTTONS)
+  // update profile information
+  @PutMapping("/{id}/update/info")
+  public ResponseEntity<Users> updateProfile(@RequestBody Users user, @PathVariable("id") int userId) {
+    Users updatedProfile = userService.updateProfile(user, userId);
+    return new ResponseEntity<>(updatedProfile, HttpStatus.OK);
+  }
+
+  // update profile picture
+  @PutMapping("/{id}/update/picture")
+  public ResponseEntity<Users> updatePicture(@RequestBody Users user, @PathVariable("id") int userId) {
+    Users newPic = userService.updateProfilePic(user, userId);
+    return new ResponseEntity<>(newPic, HttpStatus.OK);
+  }
+
+  // update status
+  @PutMapping("/{id}/update/status")
+  public ResponseEntity<Users> updateStatus(@RequestBody Users user, @PathVariable("id") int userId) {
+    Users newStatus = userService.updateStatus(user, userId);
+    return new ResponseEntity<>(newStatus, HttpStatus.OK);
+  }
+
+  // update profile bio
+  @PutMapping("/{id}/update/bio")
+  public ResponseEntity<Users> updateBio(@RequestBody Users user, @PathVariable("id") int userId) {
+    Users newBio = userService.updateBio(user, userId);
+    return new ResponseEntity<>(newBio, HttpStatus.OK);
+  }
+
+
+  // ORIGINAL METHODS - before refactoring to add @PathVariable("id")
+//  // add new profile
+//  @PostMapping("/add")
+//  public ResponseEntity<Users> addProfile(@RequestBody Users user, String first, String last, String display, String email, String location, String pic, String status, String bio) {
+//    Users newProfile = userService.addProfile(user, first, last, display, email, location, pic, status, bio);
+//    return new ResponseEntity<>(newProfile, HttpStatus.CREATED);
+//  }
+//
+//  // update profile information
+//  @PutMapping("/update/info")
+//  public ResponseEntity<Users> updateProfile(@RequestBody Users user, String first, String last, String display, String email, String location) {
+//    Users updatedProfile = userService.updateProfile(user, first, last, display, email, location);
+//    return new ResponseEntity<>(updatedProfile, HttpStatus.OK);
+//  }
+//
+//  // update profile picture
+//  @PutMapping("/update/picture")
+//  public ResponseEntity<Users> updatePicture(@RequestBody Users user, String pic) {
+//    Users newPic = userService.updateProfilePic(user, pic);
+//    return new ResponseEntity<>(newPic, HttpStatus.OK);
+//  }
+//
+//  // update status
+//  @PutMapping("/update/status")
+//  public ResponseEntity<Users> updateStatus(@RequestBody Users user, String status) {
+//    Users newStatus = userService.updateStatus(user, status);
+//    return new ResponseEntity<>(newStatus, HttpStatus.OK);
+//  }
+//
+//  // update profile bio
+//  @PutMapping("/update/bio")
+//  public ResponseEntity<Users> updateBio(@RequestBody Users user, String bio) {
+//    Users newBio = userService.updateBio(user, bio);
+//    return new ResponseEntity<>(newBio, HttpStatus.OK);
+//  }
+
 
 // PROFILE ADDED DURING REGISTRATION PROCESS
 //  // add new profile
--- MyHabit/MyHabit/src/main/java/com/MyHabit/MyHabit/repositories/UserRepo.java
+++ MyHabit/MyHabit/src/main/java/com/MyHabit/MyHabit/repositories/UserRepo.java
@@ -4,6 +4,7 @@ import com.MyHabit.MyHabit.models.Users;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -13,4 +14,6 @@ public interface UserRepo extends CrudRepository<Users, Integer> {
   void deleteUsersById(int id);
 
   Optional<Users> findUsersById(int id); // optional in case no user w given id
+
+  List<Users> findAll();
 }
