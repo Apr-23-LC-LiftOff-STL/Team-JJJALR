d949de5d8a03c2e41c6d6f21ee29ed0d58145fc8
julian <julianschriedel@gmail.com>
2023-06-10 13:01:56 -0500

    my files just re-loaded weird and i needed to remove duplicate files in edit-profile component

---
+++ MyHabit/MyHabit/src/main/ui/src/app/edit-profile/edit-profile.component 2.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { ProfileEdit } from './edit-profile';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { EditProfileService } from './edit-profile.service';
+import { NgForm } from '@angular/forms';
+
+
+// Add a component decorator - identifies the class as a component
+// Choose a CSS selector - tells angular to instantiate the component where tag is found in html file
+// Define the template - tells angular how to render the component
+// Declare component styles by calling CSS file
+
+// COMPONENT DECORATOR
+@Component({
+  selector: 'app-edit-profile',
+  templateUrl: './edit-profile.component.html',
+  styleUrls: ['./edit-profile.component.css'],
+})
+
+export class EditProfileComponent implements OnInit {
+
+  // PROPERTIES
+  public profileEdit!: ProfileEdit;
+  public editProfileForm!: NgForm;
+
+
+  // CONSTRUCTOR
+  constructor(private editProfileService: EditProfileService, private http: HttpClient, private router: Router) {
+    this.http = http;
+    this.router = router;
+  }
+
+  ngOnInit(): void {
+    // when the component is initialized it will call the functions below
+    
+    // this.profileEdit.id;
+    // this.profileEdit;
+    // this.onUpdateProfile(this.editProfileForm);
+  }
+
+  public onUpdateProfile(editProfileForm:  NgForm): void {
+    this.editProfileService.updateProfile(editProfileForm.value, this.profileEdit.id).subscribe(
+      (response: ProfileEdit) => {
+        this.profileEdit = response;
+        this.router.navigate(['info'])
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    )
+  }
+
+}
